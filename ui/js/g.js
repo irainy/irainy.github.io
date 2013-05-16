84b4d955db56c7e29724c5bf0bf510c883f64bd7
me@mac <sniky.hack@gmail.com>
2013-05-16 22:53:25 +0800

    mv drafts to v0

---
+++ ui/js/g.js
@@ -0,0 +1,15 @@
+/*$(document).ready(function() {
+	$("#side-btns").click(function(){
+		$("#main-layer").animate({
+			left: "200px"
+		}, 100, 'linear').clearQueue();
+
+	});
+	$("#main").click(function(){
+			$("#main-layer").animate({
+				left: "0px"
+			}, 100, 'linear');
+	});
+
+})
+*/
