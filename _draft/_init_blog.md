91a5249ed66432af6591d4a437d89dc884e5a66c
rainy <rainy.sun@fu101.com>
2013-10-22 13:34:54 +0800

    clear

--- _draft/_init_blog.md
+++
@@ -1,7 +0,0 @@
-<conf style='display:none'>
-title: TITLE
-permalink: http://sniky.github.io/article/TITLE.html
-tags: 
-author: rainy
-datetime: yyyymmddhhmm
-</conf>
--- _tools/db/sniky.github.io.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS "article";
-CREATE TABLE "article" ("id" INTEGER PRIMARY KEY  NOT NULL , "title" CHAR NOT NULL  UNIQUE , "create_date" DATETIME NOT NULL , "last_update" DATETIME NOT NULL , "posted" BOOL NOT NULL  DEFAULT 0, "cate_name" VARCHAR NOT NULL , "permalink" CHAR NOT NULL, "brief" TEXT);
